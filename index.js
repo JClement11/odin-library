f899be9abc71df2311666cde0027b11441384865
JClement11 <jclement0829@gmail.com>
2024-10-29 18:32:37 -0500

    Create array for library with dummy text

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const myLibrary = [];
+const myLibrary = [
+    { title: "Book1", author: "Author1", pages: 100, read: "Read" },
+    { title: "Book2", author: "Author2", pages: 200, read: "Read" },
+    { title: "Book3", author: "Author3", pages: 300, read: "Not Read" }
+];
 
 function Book(title, author, pages, read) {
     this.title = title;
