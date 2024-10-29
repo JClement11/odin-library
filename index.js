e05a089e58d57911056f3ead9d43e0df624e94cb
JClement11 <jclement0829@gmail.com>
2024-10-29 18:33:08 -0500

    Create function to convert array to table

--- index.js
+++ index.js
@@ -14,3 +14,38 @@ function Book(title, author, pages, read) {
 function addBookToLibrary() {
 
 }
+
+let myTable = document.querySelector("#table");
+
+function createTable(tableData) {
+    let table = document.createElement("table");
+    let headerRow = document.createElement("tr");
+
+    let headers = ["Title", "Author", "Pages", "Read?"];
+
+    headers.forEach(headerText => {
+        let header = document.createElement("th");
+        let textNode = document.createTextNode(headerText);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    });
+
+    table.appendChild(headerRow);
+
+    tableData.forEach(data => {
+        let row = document.createElement("tr");
+
+        Object.values(data).forEach(text => {
+            let cell = document.createElement("td");
+            let textNode = document.createTextNode(text);
+            cell.appendChild(textNode);
+            row.appendChild(cell);
+        });
+
+        table.appendChild(row);
+    });
+
+    myTable.appendChild(table);
+}
+
+createTable(myLibrary);
