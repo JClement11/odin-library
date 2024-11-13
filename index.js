cf318c5a723b3d3fac09d9c381494f386ddd480c
JClement11 <jclement0829@gmail.com>
2024-11-13 17:27:49 -0600

    Remove default books in library

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const myLibrary = [
-    { title: "Book1", author: "Author1", pages: 100, read: "Read" },
-    { title: "Book2", author: "Author2", pages: 200, read: "Read" },
-    { title: "Book3", author: "Author3", pages: 300, read: "Not Read" }
-];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
